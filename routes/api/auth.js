88f3d8aa21a51d3bc57dfe0b3d2dc18b4a4b0d8b
himanshu-thakur-7 <himan7781@gmail.com>
2020-12-02 16:00:55 +0530

    Added login functionality

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,12 @@ const router=express.Router()
 const auth=require('../../middleware/auth')
 const User = require('../../models/User')
 
+const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
+const config=require('config')
+
+const validator=require('validator')
+
 // @route  GET api/auth
 // @desc   Test
 // @access Protected(jwt required)
@@ -19,5 +25,60 @@ router.get('/',auth,async(req,res)=>{
    }
 
 })
+// @route  POST api/users
+// @desc   Register user
+// @access Public (no jwt required)
+router.post('/',async (req,res)=>{
+    const {email,password}=req.body
+    const iserror=!validator.isEmail(email || '') || !validator.isLength(password || '',1)
+    // const iserror=false
+    if (iserror){
+        return res.status(400).send('Error')
+    }
+
+    try{
+    
+    let user= await User.findOne({email})
+    if (!user){
+     return   res.status(400).send('Invalid credentials')
+    }
+    
+   // match passwords
+   const isMatch = await bcrypt.compare(password,user.password)
+
+    if (!isMatch){
+     return   res.status(400).send('Invalid credentials')
+    }
+
+
+
+
+   const payload={
+       user:{
+           id: user.id
+       }
+   }
+   jwt.sign(payload,config.get('jsonSecretKey'),
+   
+   {expiresIn:360000},
+   (err,token)=>{
+     if (err) throw err;
+     res.json({token})
+   }
+   )
+
+
+
+// return jsonWebToken
+}
+catch(err){
+  console.error(err.message)
+  res.status(500).send('Server Error')
+
+}
+
+}
+
+)
 
 module.exports = router
