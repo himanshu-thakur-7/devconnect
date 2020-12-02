d7053ab3636719b18325a238e5091ef8665bc0fd
himanshu-thakur-7 <himan7781@gmail.com>
2020-12-02 15:38:29 +0530

    Added authorization route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const router=express.Router()
 const auth=require('../../middleware/auth')
+const User = require('../../models/User')
 
 // @route  GET api/auth
 // @desc   Test
 // @access Protected(jwt required)
-router.get('/',auth,(req,res)=>res.send('Auth Route'))
+router.get('/',auth,async(req,res)=>{
+
+   try{
+    
+    const user=await User.findById(req.user.id).select('-password')
+    res.json(user)
+   }
+   catch(err){
+   console.log(err.message)
+   res.status(401).json({err:err.message})
+   }
+
+})
 
 module.exports = router
